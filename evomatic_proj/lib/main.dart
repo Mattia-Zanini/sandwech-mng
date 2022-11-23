e2668e70340e8bfb535e41ca7d773119cbe3086d
Mattia-Zanini <mattzanini2@gmail.com>
2022-11-23 16:49:22 +0100

    prove con flutter

--- evomatic_proj/lib/main.dart
+++ evomatic_proj/lib/main.dart
@@ -7,24 +7,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'App Demo',
+      theme: ThemeData(brightness: Brightness.light, useMaterial3: true),
+      darkTheme: ThemeData(brightness: Brightness.dark, useMaterial3: true),
+      themeMode: ThemeMode.system, // ThemeMode.system to follow system theme
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App Demo'),
+        ),
+        body: const Center(
+          child: Text('Ah bello de mamma'),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
