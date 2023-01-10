550313dd9039655d36cb807e80478988898edf63
Mattia-Zanini <mattzanini2@gmail.com>
2023-01-10 16:18:20 +0100

    init Sandwech MNG website

---
+++ js/tables.js
@@ -0,0 +1,29 @@
+var data = [
+    {
+        "name": "Tiger Nixon",
+        "position": "System Architect",
+        "salary": "$3,120",
+        "start_date": "2011/04/25",
+        "office": "Edinburgh",
+        "extn": "5421"
+    },
+    {
+        "name": "Garrett Winters",
+        "position": "Director",
+        "salary": "$5,300",
+        "start_date": "2011/07/25",
+        "office": "Edinburgh",
+        "extn": "8422"
+    }
+];
+$(document).ready(function () {
+    $('#example').DataTable({
+        data: data,
+        columns: [
+            { data: 'name' },
+            { data: 'position' },
+            { data: 'salary' },
+            { data: 'office' }
+        ]
+    });
+});
