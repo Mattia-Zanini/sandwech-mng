ecfedf9968b76bd2f83dbe7251f7032ce4bb6064
Mattia-Zanini <mattzanini2@gmail.com>
2023-01-13 17:25:02 +0100

    setup Pagina principale

--- wp-content/themes/Sandwech/front-page.php
+++ wp-content/themes/Sandwech/front-page.php
@@ -1,20 +1,80 @@
 <?php get_header(); ?>
 
-<div class="container-fluid front-page-content">
+<div class="container-fluid">
+    <!--
     <div class="row">
         <div class="col-12">
-            <h1 class="title text-center"><?php echo get_the_title() ?></h1>
+            <h1 class="title text-center"><?php //echo get_the_title(); ?></h1>
             <hr />
         </div>
     </div>
+    -->
 
     <div class="row">
-        <div class="col-2"></div>
-        <div class="col-8 text-center">
-            <?php echo the_content() ?>
+        <div class="col-2">
+            <!--
+            [
+                "allergen",
+                "break",
+                "cart",
+                "class",
+                "favourite",
+                "ingredient",
+                "nutritional_value",
+                "offer",
+                "order",
+                "pickup",
+                "pickup_break",
+                "product",
+                "product_allergen",
+                "product_ingredient",
+                "product_offer",
+                "product_order",
+                "product_tag",
+                "reset",
+                "status",
+                "tag",
+                "user",
+                "user_class"
+            ]
+            -->
+            <ul id="tables_list">
+                <li><a href="http://localhost/sandwech-mng">Coffee</a></li>
+                <li>Tea</li>
+                <li>Milk</li>
+            </ul>
         </div>
-        <div class="col-2"></div>
+        <div class="col-10">
+            <div class="row">
+                <h1 class="title text-center" id="title_table">
+                    <?php echo get_the_title(); ?>
+                </h1>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <table id="table" class="table table-striped" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Description</th>
+                                <th>Quantity</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+
+<script type="text/javascript">
+$(document).ready(function() {
+    var tableName = $('#table_name').text();
+    //console.log(tableName);
+    $('#title_table').text(tableName);
+});
+</script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/js/table.js"></script>
 
 <?php get_footer(); ?>
--- wp-content/themes/Sandwech/header.php
+++ wp-content/themes/Sandwech/header.php
@@ -15,12 +15,13 @@
     <header class="sticky-top">
         <nav class="navbar navbar-expand-lg navbar-color">
             <div class="container-fluid">
-                <a class="navbar-brand mb-0 h1" href="http://localhost/sandwech-mng">Sandwech</a>
+                <a class="navbar-brand mb-0 h1" href="http://localhost/sandwech-mng">Sandwech Management</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                <!--
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
@@ -28,6 +29,8 @@
                         </li>
                     </ul>
                 </div>
+                -->
             </div>
         </nav>
     </header>
+    <h1 hidden id="table_name">product</h1> <!-- tabella "product" come predefinita -->
