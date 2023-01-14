aec1bd548a5786f083ae6cab896705b0de2e1a37
Mattia-Zanini <mattzanini2@gmail.com>
2023-01-14 17:13:55 +0100

    alcuni commenti

--- wp-content/themes/Sandwech/functions.php
+++ wp-content/themes/Sandwech/functions.php
@@ -1,4 +1,24 @@
 <?php
+/*
+Il file functions.php è un file presente all'interno di ogni tema di WordPress e serve a specificare le 
+funzionalità e le personalizzazioni del tema. Il file functions.php è un file PHP, quindi può contenere 
+codice PHP, ma anche codice HTML, CSS e JavaScript.
+Il file functions.php è un file importante perché permette di aggiungere funzionalità al tuo tema, senza 
+modificare il codice del tema stesso. Ciò significa che, se si decide di cambiare tema in futuro, le personalizzazioni 
+create nel file functions.php non verranno perse.
+Il file functions.php può essere utilizzato per:
+Aggiungere funzionalità al tuo tema, come ad esempio la creazione di un menu personalizzato, l'aggiunta di 
+widget personalizzati o la creazione di shortcode personalizzati.
+Personalizzare il comportamento predefinito di WordPress, come ad esempio la modifica della lunghezza 
+dell'estratto dei post o la modifica della lunghezza del titolo della pagina.
+Caricare risorse esterne, come ad esempio i file CSS o JavaScript.
+Eseguire qualsiasi altra operazione che si desidera eseguire prima che il tema venga caricato.
+In sintesi, il file functions.php è un file presente in ogni tema di wordpress, permette di aggiungere 
+funzionalità e personalizzazioni al tema senza modificare il codice del tema stesso, può contenere diversi 
+tipi di codice e permette di personalizzare il comportamento predefinito di wordpress.
+*/
+
+
 //carica correttamente i file .css
 function load_stylesheets()
 {
--- wp-content/themes/Sandwech/js/table_Example.js
+++ wp-content/themes/Sandwech/js/table_Example.js
@@ -1,10 +1,35 @@
+/*
+Questo file javascript utilizza la libreria jQuery e la libreria DataTables per creare una tabella dinamica e interattiva 
+per la visualizzazione e la modifica dei dati.
+
+La prima riga del file utilizza la notazione "$" per accedere a jQuery, che è una libreria javascript 
+che semplifica la scrittura di codice javascript per la manipolazione del DOM e la gestione degli eventi.
+
+La funzione on('load') di jQuery viene utilizzata per eseguire il codice all'interno della funzione 
+quando la pagina è completamente caricata. Dentro questa funzione vengono definite varie proprietà per l'editor e la tabella.
+
+L'editor viene creato utilizzando la funzione Editor() della libreria DataTables, con proprietà come l'URL dell'API, 
+l'identificatore della tabella e i campi da visualizzare e modificare. La tabella viene creata utilizzando la 
+funzione DataTable() della libreria DataTables, con proprietà come l'URL dell'API, le colonne da visualizzare, 
+la possibilità di selezionare righe e l'utilizzo di bottoni per la creazione, modifica e rimozione di dati.
+
+In fine, vengono creati i bottoni per la creazione, modifica e rimozione di dati tramite la funzione Buttons() 
+della libreria DataTables e vengono appesi alla tabella, specificando l'editor come parametro.
+*/
+
+
+// Questo codice utilizza la libreria jQuery per creare una tabella dinamica con funzionalità di modifica e cancellazione.
+// Assegnamo l'oggetto jQuery alla variabile $
 var $ = jQuery;
 
+// Aspettiamo il caricamento completo della pagina prima di eseguire il codice seguente
 $(window).on('load', function () {
+    // Creiamo un oggetto Editor per la tabella, specificando l'URL del file PHP che gestirà le richieste AJAX
     // fields dell'editor
     var editor = new $.fn.dataTable.Editor({
-        ajax: "EditorPHP/controllers/product.php",
+        ajax: "../EditorPHP/controllers/product.php",
         table: "#table",
+        // Definiamo i campi dell'editor e i loro nomi
         fields: [{
             label: "Name:",
             name: "name"
@@ -32,11 +57,13 @@ $(window).on('load', function () {
         ]
     });
 
+    // Creiamo l'oggetto DataTable per la tabella, specificando l'URL del file PHP che fornirà i dati della tabella
     // fields della tabella
     var table = $('#table').DataTable({
-        //dom: "Bfrtip",
+        // Disabilitiamo la possibilità di cambiare la lunghezza della tabella
         lengthChange: false,
-        ajax: "EditorPHP/controllers/product.php",
+        ajax: "../EditorPHP/controllers/product.php",
+        // Definiamo le colonne della tabella e i loro nomi
         columns: [{
             data: "name"
         },
@@ -50,11 +77,11 @@ $(window).on('load', function () {
             data: "quantity"
         }
         ],
+        // Abilitiamo la selezione delle righe
         select: true,
-        //scrollY: 500,
     });
 
-    // Display the buttons
+    // Aggiungiamo i pulsanti per la creazione, la modifica e la cancellazione delle righe
     new $.fn.dataTable.Buttons(table, [
         { extend: "create", editor: editor },
         { extend: "edit", editor: editor },
